4496660fcacfa73d9e62e6dbfa55b074e7787935
Sabyrzhan-Tasbolatov <novitoll@mail.ru>
2016-09-14 17:33:46 +0600

    Moved jinja2 templates close to JS controllers

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
+import os
+import jinja2
 from flask import Flask
 
+static_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+
 app = Flask(__name__)
+app.jinja_loader = jinja2.FileSystemLoader(static_folder_path)
 
 
 if __name__ == '__main__':
---
+++ ex/1.js
@@ -0,0 +1,11 @@
+(function () {
+
+  angular
+    .module('app')
+    .controller('MyCtrl', MyCtrl);
+
+  /** @ngInject */
+  function MyCtrl($scope, $http, UserService) {
+
+  }
+})();
